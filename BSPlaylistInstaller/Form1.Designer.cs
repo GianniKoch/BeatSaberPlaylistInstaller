46ea83b1e815f0b94f563f5bd6c09856de84a817
Gianni Koch <me@giannikoch.com>
2020-05-16 20:01:01 +0200

    - fixed: #3
    - Existing songs won't be downloaded

--- BSPlaylistInstaller/Form1.Designer.cs
+++ BSPlaylistInstaller/Form1.Designer.cs
@@ -28,38 +28,110 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.txtLog = new System.Windows.Forms.RichTextBox();
+            this.cboActiveStop = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnStopDownload = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnStopAll = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(9, 10);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(557, 71);
+            this.button1.Size = new System.Drawing.Size(362, 43);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Select playlist";
+            this.button1.Text = "Select a beatsaver playlist (.json or .bplist)";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // txtLog
             // 
-            this.txtLog.Location = new System.Drawing.Point(12, 98);
+            this.txtLog.Location = new System.Drawing.Point(376, 10);
+            this.txtLog.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtLog.Name = "txtLog";
-            this.txtLog.Size = new System.Drawing.Size(557, 381);
+            this.txtLog.Size = new System.Drawing.Size(384, 389);
             this.txtLog.TabIndex = 2;
             this.txtLog.Text = "";
             // 
+            // cboActiveStop
+            // 
+            this.cboActiveStop.FormattingEnabled = true;
+            this.cboActiveStop.Location = new System.Drawing.Point(192, 64);
+            this.cboActiveStop.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.cboActiveStop.Name = "cboActiveStop";
+            this.cboActiveStop.Size = new System.Drawing.Size(92, 21);
+            this.cboActiveStop.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 67);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(156, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Select active download to stop:";
+            // 
+            // btnStopDownload
+            // 
+            this.btnStopDownload.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStopDownload.Location = new System.Drawing.Point(287, 58);
+            this.btnStopDownload.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnStopDownload.Name = "btnStopDownload";
+            this.btnStopDownload.Size = new System.Drawing.Size(84, 30);
+            this.btnStopDownload.TabIndex = 5;
+            this.btnStopDownload.Text = "Stop";
+            this.btnStopDownload.UseVisualStyleBackColor = true;
+            this.btnStopDownload.Click += new System.EventHandler(this.btnStopDownload_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(90, 104);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Stop all downloads:";
+            // 
+            // btnStopAll
+            // 
+            this.btnStopAll.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStopAll.Location = new System.Drawing.Point(192, 95);
+            this.btnStopAll.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnStopAll.Name = "btnStopAll";
+            this.btnStopAll.Size = new System.Drawing.Size(91, 30);
+            this.btnStopAll.TabIndex = 8;
+            this.btnStopAll.Text = "Stop All";
+            this.btnStopAll.UseVisualStyleBackColor = true;
+            this.btnStopAll.Click += new System.EventHandler(this.btnStopAll_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(583, 491);
+            this.ClientSize = new System.Drawing.Size(767, 408);
+            this.Controls.Add(this.btnStopAll);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnStopDownload);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboActiveStop);
             this.Controls.Add(this.txtLog);
             this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
-            this.Text = "bspl";
+            this.Text = "Beat Saber Playlist Installer";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -67,6 +139,11 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.RichTextBox txtLog;
+        private System.Windows.Forms.ComboBox cboActiveStop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnStopDownload;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnStopAll;
     }
 }
 
